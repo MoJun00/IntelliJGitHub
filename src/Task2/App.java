9a632118286c52af89a8b769e474a6ae110b1cb1
MoJun00 <167170478+MoJun00@users.noreply.github.com>
2024-04-29 14:53:26 +0900

    양의 정수 2개(0 포함)와 연산 기호를 매개변수로 받아 사칙연산(+,-,*,/) 기능을 수행한 후 결과 값을 반환하는 메서드와 연산 결과를 저장하는 컬렉션 타입 필드를 가진 Calculator 클래스를 생성합니다. Level 1에서 구현한 App 클래스의 main 메서드에 Calculator 클래스가 활용될 수 있도록 수정합니다.

---
+++ src/Task2/App.java
@@ -0,0 +1,35 @@
+package Task2;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class App {
+    public static void main(String[] args) {
+        Calculator calculator = new Calculator();
+
+        Scanner sc = new Scanner(System.in);
+
+        while (true) {
+            System.out.print("첫 번째 숫자를 입력하세요 : ");
+            int num1 = sc.nextInt();
+            System.out.print("두 번째 숫자를 입력하세요 : ");
+            int num2 = sc.nextInt();
+
+            System.out.print("사칙연산 기호를 입력하세요 : ");
+            char operator = sc.next().charAt(0);
+
+            try {
+                calculator.calculate(num1,num2,operator);
+            } catch (Exception e) {
+                System.out.println("에러 발생!!");
+                System.out.println(e.getMessage());
+            }
+
+            System.out.println("더 계산하시겠습니까? (exit 입력 시 종료)");
+            /* exit을 입력 받으면 반복 종료 */
+            String answer = sc.next();
+            if(answer.equalsIgnoreCase("exit"))
+                break;
+        }
+    }
+}
---
+++ src/Task2/Calculator.java
@@ -0,0 +1,38 @@
+package Task2;
+
+import java.util.ArrayList;
+
+public class Calculator {
+    /* 연산 결과를 저장하는 컬렉션 타입 필드 선언 및 생성 */
+    ArrayList<Double> arrayResults = new ArrayList<Double>();
+
+    public double calculate(double a, double b, char operator) {
+        /* 위 요구사항에 맞게 구현 */
+        /* return 연산 결과 */
+        double c=0;
+
+        if (operator == '+') {
+            c=a+b;
+        }
+        else if (operator == '-') {
+            c=a-b;
+        }
+        else if (operator == '*') {
+            c=a*b;
+        }
+        else if (operator == '/') {
+            if(b==0)
+                throw new RuntimeException("나눗셈 연산에서 분모(두번째 정수)에 0이 입력될 수 없습니다.");
+                //throw new ArithmeticException("Division by zero");
+            //throw new RuntimeException(e);
+            c=a/b;
+        }
+        else
+        {
+            throw new RuntimeException("적합한 연산자 기호가 아닙니다.");
+        }
+
+        arrayResults.add(c);
+        return c;
+    }
+}
