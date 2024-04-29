554fd1a2ca4d3f94dd2bde2d50ceea7b18bd0296
MoJun00 <167170478+MoJun00@users.noreply.github.com>
2024-04-29 13:41:20 +0900

    양의 정수는 각각 하나씩 전달 받습니다.

--- src/Task1/App.java
+++ src/Task1/App.java
@@ -1,7 +1,16 @@
 package Task1;
 
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) {
-        System.out.println("test2");
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("첫 번째 숫자를 입력하세요 : ");
+        int firstNum = sc.nextInt();
+        // Scanner를 사용하여 양의 정수를 입력받고 적합한 타입의 변수에 저장합니다.
+        System.out.print("두 번째 숫자를 입력하세요 : ");
+        int secondNum = sc.nextInt();
+        // Scanner를 사용하여 양의 정수를 입력받고 적합한 타입의 변수에 저장합니다.
     }
 }
