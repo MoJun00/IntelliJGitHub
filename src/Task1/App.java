570f84ab79473686b6be79d52f2f4b5c6bc29165
MoJun00 <167170478+MoJun00@users.noreply.github.com>
2024-04-29 13:56:02 +0900

    반복문을 사용하여 반복의 종료를 알려주는 “exit” 문자열을 입력하기 전까지 무한으로 계산을 진행할 수 있도록 소스 코드를 수정합니다.

--- src/Task1/App.java
+++ src/Task1/App.java
@@ -6,34 +6,40 @@ public class App {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        System.out.print("첫 번째 숫자를 입력하세요 : ");
-        int firstNum = sc.nextInt();
-        // Scanner를 사용하여 양의 정수를 입력받고 적합한 타입의 변수에 저장합니다.
-        System.out.print("두 번째 숫자를 입력하세요 : ");
-        int secondNum = sc.nextInt();
-        // Scanner를 사용하여 양의 정수를 입력받고 적합한 타입의 변수에 저장합니다.
+        while (true) {
+            System.out.print("첫 번째 숫자를 입력하세요 : ");
+            int firstNum = sc.nextInt();
+            // Scanner를 사용하여 양의 정수를 입력받고 적합한 타입의 변수에 저장합니다.
+            System.out.print("두 번째 숫자를 입력하세요 : ");
+            int secondNum = sc.nextInt();
+            // Scanner를 사용하여 양의 정수를 입력받고 적합한 타입의 변수에 저장합니다.
 
-        System.out.print("사칙연산 기호를 입력하세요: ");
-        // 사칙연산 기호를 적합한 타입으로 선언한 변수에 저장합니다.
-        String operation = sc.next();
+            System.out.print("사칙연산 기호를 입력하세요: ");
+            // 사칙연산 기호를 적합한 타입으로 선언한 변수에 저장합니다.
+            String operation = sc.next();
 
-        double result = 0;
-        /* 제어문을 활용하여 위 요구사항을 만족할 수 있게 구현합니다.*/
-        if (operation.equals("+"))
-            result = firstNum + secondNum;
-        else if (operation.equals("-"))
-            result = firstNum - secondNum;
-        else if (operation.equals("*"))
-            result = firstNum * secondNum;
-        else if (operation.equals("/"))
-        {
-            if(secondNum == 0) {
-                System.out.println("나눗셈 연산에서 분모(두번째 정수)에 0이 입력될 수 없습니다.");
-                result = 0;
+            double result = 0;
+            /* 제어문을 활용하여 위 요구사항을 만족할 수 있게 구현합니다.*/
+            if (operation.equals("+"))
+                result = firstNum + secondNum;
+            else if (operation.equals("-"))
+                result = firstNum - secondNum;
+            else if (operation.equals("*"))
+                result = firstNum * secondNum;
+            else if (operation.equals("/")) {
+                if (secondNum == 0) {
+                    System.out.println("나눗셈 연산에서 분모(두번째 정수)에 0이 입력될 수 없습니다.");
+                    result = 0;
+                } else
+                    result = (double) firstNum / secondNum;
             }
-            else
-                result = (double) firstNum / secondNum;
+            System.out.println("결과 : " + result);
+
+            System.out.println("더 계산하시겠습니까? (exit 입력 시 종료)");
+            /* exit을 입력 받으면 반복 종료 */
+            String answer = sc.next();
+            if(answer.equals("exit"))
+                break;
         }
-        System.out.println("결과 : " + result);
     }
 }
