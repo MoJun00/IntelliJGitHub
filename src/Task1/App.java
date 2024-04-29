dfbb6c8d993481c519e7131a1ee9d2ff4a8becc6
MoJun00 <167170478+MoJun00@users.noreply.github.com>
2024-04-29 14:15:11 +0900

    연산 결과가 10개로 고정되지 않고 무한이 저장될 수 있도록 소스 코드를 수정합니다.

--- src/Task1/App.java
+++ src/Task1/App.java
@@ -1,14 +1,13 @@
 package Task1;
 
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class App {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        /* 연산의 결과를 저장할 수 있도록 적합한 타입의 배열을 생성합니다. */
-        /* 연산의 결과가 저장된 배열의 마지막 index를 저장하는 변수를 선언 */
-        double[] results = new double[10];
+        ArrayList<Double> arrayResults = new ArrayList<Double>();
         int cnt = 0;
 
         while (true) {
@@ -19,12 +18,11 @@ public class App {
             int secondNum = sc.nextInt();
             // Scanner를 사용하여 양의 정수를 입력받고 적합한 타입의 변수에 저장합니다.
 
-            System.out.print("사칙연산 기호를 입력하세요: ");
-            // 사칙연산 기호를 적합한 타입으로 선언한 변수에 저장합니다.
+            System.out.print("사칙연산 기호를 입력하세요 : ");
             String operation = sc.next();
 
             double result = 0;
-            /* 제어문을 활용하여 위 요구사항을 만족할 수 있게 구현합니다.*/
+
             if (operation.equals("+"))
                 result = firstNum + secondNum;
             else if (operation.equals("-"))
@@ -39,22 +37,17 @@ public class App {
                     result = (double) firstNum / secondNum;
             }
             System.out.println("결과 : " + result);
+            arrayResults.add(result);
 
-            if (cnt != 10) {
-                results[cnt] = result;
-                cnt++;
-            }
-            else {
-                for(int i = 1; i < cnt; i++) {
-                    results[i-1] = results[i];
-                }
-                results[cnt-1] = result;
-            }
+            System.out.println("가장 먼저 저장된 연산 결과를 삭제하시겠습니까? (remove 입력 시 삭제)");
+            String answer1 = sc.next();
+            if(answer1.equalsIgnoreCase("remove"))
+                arrayResults.remove(0);
 
             System.out.println("더 계산하시겠습니까? (exit 입력 시 종료)");
             /* exit을 입력 받으면 반복 종료 */
-            String answer = sc.next();
-            if(answer.equals("exit"))
+            String answer2 = sc.next();
+            if(answer2.equalsIgnoreCase("exit"))
                 break;
         }
     }
